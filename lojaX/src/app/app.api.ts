782a011e264f8d38c2ff2221961fbad242c61caf
Frankika <mfrankika@gmail.com>
2019-06-12 23:13:32 +0100

    atualizar

--- lojaX/src/app/app.api.ts
+++
@@ -1,4 +0,0 @@
-export const URL_OFERTAS = 'http://localhost:3000/ofertas';
-export const URL_COMO_USAR = 'http://localhost:3000/como-usar';
-export const URL_ONDE_FICA = 'http://localhost:3000/onde-fica';
-export const URL_PEDIDOS = 'http://localhost:3000/pedidos';
